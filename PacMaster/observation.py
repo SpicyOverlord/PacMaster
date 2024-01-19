060ed9a6409f30dca26fad2fcb9611d84767a4b2
SpicyOverlord <spicyoverlord@gmail.com>
2024-01-19 15:47:57 +0100

    update Observation class with return types

--- PacMaster/observation.py
+++ PacMaster/observation.py
@@ -1,6 +1,6 @@
 import math
 from Pacman_Complete.ghosts import *
-from Pacman_Complete.nodes import Node
+from Pacman_Complete.nodes import Node, NodeGroup
 
 
 class Observation(object):
@@ -10,39 +10,40 @@ class Observation(object):
         self.nodes = game.nodes
         self.pacman = game.pacman
 
-      # ------------------ Pacman Functions ------------------
-    def getPacmanPosition(self):
+    # ------------------ Pacman Functions ------------------
+    def getPacmanPosition(self) -> Vector2:
         return self.pacman.position
 
     # ------------------ Node Functions ------------------
-    def getNodes(self):
+    def getNodeGroup(self) -> NodeGroup:
         return self.nodes
 
     def getNoteFromTiles(self, x, y) -> Node:
         return self.nodes.getNoteFromTiles((x, y))
 
     # ------------------ Pellet Functions ------------------
-    def getPelletPositions(self):
+    def getPelletPositions(self) -> list[Vector2]:
         return [pellet.position for pellet in self.pelletGroup.pelletList]
 
-    def getPowerPelletPositions(self):
+    def getPowerPelletPositions(self) -> list[Vector2]:
         return [powerPellet.position for powerPellet in self.pelletGroup.powerpellets]
 
-    def getClosestPelletPosition(self):
+    def getClosestPelletPosition(self) -> Vector2:
         return min(self.getPelletPositions(), key=lambda p: self.squaredDistance(p, self.pacman.position), default=None)
 
-    def getClosestPowerPelletPosition(self):
-        return min(self.getPowerPelletPositions(), key=lambda p: self.squaredDistance(p, self.pacman.position), default=None)
+    def getClosestPowerPelletPosition(self) -> Vector2:
+        return min(self.getPowerPelletPositions(), key=lambda p: self.squaredDistance(p, self.pacman.position),
+                   default=None)
 
     # ------------------ Ghost Functions ------------------
-    def getGhostMode(self):
+    def getGhostMode(self) -> int:
         return self.ghostGroup.blinky.mode.current
 
-    def getGhostPositions(self):
+    def getGhostPositions(self) -> list[Vector2]:
         return [self.ghostGroup.blinky.position, self.ghostGroup.pinky.position, self.ghostGroup.inky.position,
                 self.ghostGroup.clyde.position]
 
-    def getClosestGhostPosition(self):
+    def getClosestGhostPosition(self) -> Vector2:
         return min(self.getGhostPositions(), key=lambda g: self.squaredDistance(g, self.pacman.position), default=None)
 
     def getBlinky(self) -> Blinky:
@@ -58,11 +59,11 @@ class Observation(object):
         return self.ghostGroup.clyde.position
 
     # ------------------ Helper Functions ------------------
-    def roundPosition(self, vector2: Vector2):
+    def roundPosition(self, vector2: Vector2) -> Vector2:
         return Vector2(round(vector2.x), round(vector2.y))
 
-    def squaredDistance(self, a, b):
+    def squaredDistance(self, a, b) -> int:
         return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
 
-    def distance(self, a, b):
+    def distance(self, a, b) -> float:
         return math.sqrt(self.squaredDistance(a, b))
