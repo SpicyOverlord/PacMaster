d8cc94fc2d9a1a37a92a967aeb8333660ab43e7a
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-02 15:54:35 +0100

    fix run in terminal

--- runner.py
+++ runner.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the `Pacman_Complete` folder to the sys.path
+sys.path.append(os.path.abspath('Pacman_Complete'))
+
 from PacMaster.agents.CollectorAgent import CollectorAgent
 from PacMaster.agents.FirstRealAgent import FirstRealAgent
 from PacMaster.agents.ScaredAgent import ScaredAgent
@@ -8,6 +14,6 @@ from PacMaster.utils.runnerFunctions import *
 # runGameWithHuman()
 DebugHelper.disable()
 for i in range(1000):
-    stats = calculatePerformanceOverXGames(FirstRealAgent, gameCount=50, gameSpeed=15, startLevel=0, startLives=1,
+    stats = calculatePerformanceOverXGames(FirstRealAgent, gameCount=50, gameSpeed=10, startLevel=0, startLives=1,
                                            ghostsEnabled=True, freightEnabled=True, logging=False)
     print(stats)
