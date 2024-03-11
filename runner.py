395d1b56df0b315aa315b843aed4ac8e6d851aa6
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-11 20:43:28 +0100

    update

--- runner.py
+++ runner.py
@@ -25,21 +25,21 @@ def bootstrap_confidence_interval(agent, stats: list[GameStats], confidence_leve
 
 if __name__ == "__main__":
     DebugHelper.disable()
-    # stats = calculatePerformanceOverXGames(
-    #     agentClass=MyFirstAgent,
-    #     weightContainer=WeightContainer({'fleeThreshold': 0.142, 'pelletLevelDistance': 2.699, 'wayTooCloseThreshold': 60.868, 'tooCloseThreshold': 0.263, 'tooFarAwayThreshold': 1769.249, 'wayTooCloseValue': 864.227, 'tooCloseValue': 47.85, 'dangerZoneMultiplier': 0.485, 'dangerZoneMiddleMapNodeMultiplier': 0.043, 'ghostInDangerZoneMultiplier': 0.002, 'closestGhostMultiplier': 0.579, 'ghostIsCloserMultiplier': 8.302, 'edgeMultiplier': 2.0}),
-    #     # weightContainer=None,
-    #     gameCount=200,
-    #     gameSpeed=1,
-    #     startLevel=0,
-    #     startLives=1,
-    #     ghostsEnabled=True,
-    #     freightEnabled=True,
-    #     lockDeltaTime=False,
-    #     logging=True
-    # )
-    #
-    # exit()
+    stats = calculatePerformanceOverXGames(
+        agentClass=MyFirstAgent,
+        weightContainer=WeightContainer({'fleeThreshold': 0.142, 'pelletLevelDistance': 2.699, 'wayTooCloseThreshold': 60.868, 'tooCloseThreshold': 0.263, 'tooFarAwayThreshold': 1769.249, 'wayTooCloseValue': 864.227, 'tooCloseValue': 47.85, 'dangerZoneMultiplier': 0.485, 'dangerZoneMiddleMapNodeMultiplier': 0.043, 'ghostInDangerZoneMultiplier': 0.002, 'closestGhostMultiplier': 0.579, 'ghostIsCloserMultiplier': 8.302, 'edgeMultiplier': 2.0}),
+        # weightContainer=None,
+        gameCount=500,
+        gameSpeed=1,
+        startLevel=0,
+        startLives=3,
+        ghostsEnabled=True,
+        freightEnabled=True,
+        lockDeltaTime=False,
+        logging=True
+    )
+
+    exit()
 
     for j in range(100):
         statList = []
