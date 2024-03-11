128fc71d5a2df3179a9b9ed433147feb82acb82c
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-11 08:08:13 +0100

    update

--- runner.py
+++ runner.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
         agentClass=MyFirstAgent,
         weightContainer=WeightContainer({'fleeThreshold': 0.05, 'pelletLevelDistance': 15.939, 'wayTooCloseThreshold': 16.024, 'tooCloseThreshold': 71.731, 'tooFarAwayThreshold': 2069.434, 'wayTooCloseValue': 3089.918, 'tooCloseValue': 1456.791, 'dangerZoneMultiplier': 0.005, 'dangerZoneMiddleMapNodeMultiplier': 0.001, 'ghostInDangerZoneMultiplier': 5.431, 'closestGhostMultiplier': 0.144, 'ghostIsCloserMultiplier': 2.208, 'edgeMultiplier': 3.232}),
         # weightContainer=None,
-        gameCount=100,
+        gameCount=50,
         gameSpeed=1,
         startLevel=0,
         startLives=1,
         ghostsEnabled=True,
         freightEnabled=True,
-        lockDeltaTime=True,
+        lockDeltaTime=False,
         logging=True
     )
