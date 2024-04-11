caf91f14a8d9238ef5b1adb1edb4105b0e698a6b
SpicyOverlord <SpicyOverlord@gmail.com>
2024-04-11 16:14:10 +0200

    update

--- PacmanAgentBuilder/Utils/utils.py
+++ PacmanAgentBuilder/Utils/utils.py
@@ -163,7 +163,7 @@ def save_snapshots_to_file(snapshots, fileName):
     snapshots[-1].setGameEnded()
     lastSnapshot = snapshots[-1]
 
-    snapshots = [snapshot for snapshot in snapshots if random.random() < 0.20]  # keep ~20% of the snapshots
+    snapshots = [snapshot for snapshot in snapshots if random.random() < 0.4]  # keep ~40% of the snapshots
 
     if snapshots[-1] != lastSnapshot:
         snapshots.append(lastSnapshot)
