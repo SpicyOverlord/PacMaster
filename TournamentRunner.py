8b5159a86f19ac018ade37fc045ffa34395da5ac
SpicyOverlord <SpicyOverlord@gmail.com>
2024-04-30 21:29:32 +0200

    update

--- TournamentRunner.py
+++ TournamentRunner.py
@@ -234,6 +234,6 @@ if __name__ == "__main__":
         savePercentage=10,  # The top percentile of the population to save each generation.
         mutationRate=1,  # The start mutation rate.
         gameCount=80,  # The number of games each agent will play each generation to calculate its fitness.
-        cpuCount=5,  # multiprocessing.cpu_count(),
+        cpuCount=4,  # multiprocessing.cpu_count(),
         timeoutSeconds=30 * 60  # The number of seconds to wait for each agent to finish its game before timing out.
     )
